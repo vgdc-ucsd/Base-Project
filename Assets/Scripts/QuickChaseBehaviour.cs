d17426ae33abe6a325dc361298c6bd4018aa6c1d
AccipiterChalybs <pumpkinpi64@gmail.com>
2015-09-18 23:16:57 -0700

    Added 2 extra rooms, including new Grove Kit, as well as other updates.

--- Assets/Scripts/QuickChaseBehaviour.cs
+++ Assets/Scripts/QuickChaseBehaviour.cs
@@ -11,6 +11,15 @@ public class QuickChaseBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rigidbody = this.GetComponent<Rigidbody> ();	
+		if (m_Target == null) {
+			GameObject[] targetList = GameObject.FindGameObjectsWithTag("Player");
+			foreach (GameObject tar in targetList) {
+				if (tar.GetComponent<CharacterController>() != null) {
+					m_Target = tar.transform;
+					break;
+				}
+			}
+		}
 	}
 	
 	// Update is called once per frame
